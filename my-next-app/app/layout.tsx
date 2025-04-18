6e799004facdfff99835ce9e2711e8cf35a3aaef
Param45 <paramsonawane45@gmail.com>
2025-04-18 10:14:56 +0530

    modified:   my-next-app/app/layout.tsx

--- my-next-app/app/layout.tsx
+++ my-next-app/app/layout.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Cache Visualizer",
 }
 
 export default function RootLayout({
